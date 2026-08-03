032aa497e3933ba80e2f92658c1ead177eaca1b0
nandorossetto <nandorossetto@gmail.com>
2026-08-03 11:54:50 -0300

    initial configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    ThrottlerModule.forRoot([{
+      ttl: 60000, // 1 min
+      limit: 100 // rate limit
+    }])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(helmet())
+  app.enableCors({
+    origin: process.env.CORS_ORIGN || '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowHeaders: ['Content-Type', 'Authorizarion']
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true
+    })
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Mkt api gateway')
+    .setDescription('Gtw for MS')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config); 
+  SwaggerModule.setup('api', app, document); 
+  const port = process.env.PORT || 3005;
+  await app.listen(port);
+  console.log('API Gateway running on port ', port);
+  console.log('Swagger <http://localhost>:' + port + '/api');
 }
 bootstrap();
